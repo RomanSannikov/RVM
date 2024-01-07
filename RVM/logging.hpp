aae53d030f173837dace68cc04e5842e95e8200e
Roman Sannikov <25279779+RomanSannikov@users.noreply.github.com>
2024-01-07 16:57:52 +0100

    More advanced logging

--- RVM/logging.hpp
+++ RVM/logging.hpp
@@ -4,15 +4,24 @@
 #include <concepts>
 #include <vector>
 
+#include "instruction.hpp"
+
 template<class T>
 concept Integral = std::is_integral<T>::value;
 
 class Logger {
 public:
+    static void printInstructionName(const Integral auto& instruction) {
+        #ifndef NDEBUG
+        std::cout << getInstructionName(instruction) << std::endl;
+        #endif
+    }
+
     static void printInstructions(const std::vector<Integral auto>& instructions)
 	{
-        #ifndef NDEBUG    
-        for (const auto& i : instructions) std::cout << std::bitset<8>(i) << std::endl;
+        #ifndef NDEBUG
+        for (size_t i = 0; i < instructions.size(); i++)
+            std::printf("%d: %s (%s)\n", i, std::bitset<8>(instructions[i]).to_string().c_str(), getInstructionName(instructions[i]).c_str());
         #endif
     }
 
@@ -30,4 +39,7 @@ public:
         std::cout << str << std::endl;
         #endif
     }
+
+private:
+    static std::string getInstructionName(const Integral auto& instruction) { return (instruction < c_stringInstructions.size() ? c_stringInstructions[instruction] : ""); }
 };
